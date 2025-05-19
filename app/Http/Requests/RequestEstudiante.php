1e1d56e9d822c65ebfdd47262f10138145cfd580
amatosesg <adriam.webs@gmail.com>
2025-05-19 18:04:15 +0200

    Añadir método failedValidation al Request de estudiante para controlar los errores 422 y que siempre devuelva JSON mande el usuario el Accept:application/Json o no

--- app/Http/Requests/RequestEstudiante.php
+++ app/Http/Requests/RequestEstudiante.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class RequestEstudiante extends FormRequest
 {
@@ -34,5 +36,16 @@ class RequestEstudiante extends FormRequest
         }
 
         return $reglas;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'status' => 'error',
+                'message' => 'Error de validación',
+                'errors' => $validator->errors()
+            ], 422)
+        );
     }
 }
